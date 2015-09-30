9570319cbb253cc3856452e6a1d66d5acc32c5f4
Vadym Markov <markov.vadym@gmail.com>
2015-09-30 19:02:34 +0200

    Implement CacheAware protocol

---
+++ Source/CacheAware.swift
@@ -0,0 +1,12 @@
+public protocol CacheAware {
+  var prefix: String { get }
+  var name: String { get }
+  var maxSize: UInt { get set }
+
+  init(name: String)
+
+  func add<T: AnyObject>(key: String, object: T)
+  func object<T: AnyObject>(key: String) -> T?
+  func remove(key: String)
+  func clear()
+}
