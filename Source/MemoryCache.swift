8952fe3db13936e7fd0be66325d3260b3cb63a3c
Vadym Markov <markov.vadym@gmail.com>
2015-09-30 18:58:41 +0200

    Implement Memory cache

--- Source/MemoryCache.swift
+++ Source/MemoryCache.swift
@@ -1,3 +1,33 @@
-class MemoryCache {
+import Foundation
 
+public class MemoryCache {
+  public let prefix = "no.hyper.Cache.Memory"
+
+  private let cache = NSCache()
+
+  public var maxSize: UInt = 0 {
+    didSet(value) {
+      self.cache.totalCostLimit = Int(maxSize)
+    }
+  }
+
+  public init(name: String) {
+    cache.name = prefix + name
+  }
+
+  public func add<T: AnyObject>(key: String, object: T) {
+    cache.setObject(object, forKey: key)
+  }
+
+  public func object<T: AnyObject>(key: String) -> T? {
+    return cache.objectForKey(key) as? T
+  }
+
+  public func remove(key: String) {
+    cache.removeObjectForKey(key)
+  }
+
+  public func clear() {
+    cache.removeAllObjects()
+  }
 }
