f501e7d8f59d40c2f7a197828b41d550ea932cb0
Vadym Markov <markov.vadym@gmail.com>
2015-10-09 20:02:20 +0200

    Add Cachable protocol

---
+++ Source/Cachable.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+public protocol Cachable: AnyObject {}
+
+public extension Cachable {
+  func encode() -> NSData {
+    var value = self
+    return withUnsafePointer(&value) { p in
+      NSData(bytes: p, length: sizeofValue(value))
+    }
+  }
+
+  static func decode<T: Cachable>(data: NSData) -> T {
+    let pointer = UnsafeMutablePointer<T>.alloc(sizeof(T.Type))
+    data.getBytes(pointer, length: sizeof(T))
+    return pointer.move()
+  }
+}
--- Source/DiskCache.swift
+++ Source/DiskCache.swift
@@ -17,6 +17,15 @@ public class DiskCache: CacheAware {
   // MARK: - CacheAware
 
   public func add<T: Cachable>(key: String, object: T) {
+    if !fileManager.fileExistsAtPath(path) {
+      do {
+        try fileManager.createDirectoryAtPath(path,
+          withIntermediateDirectories: true, attributes: nil)
+      } catch _ {}
+    }
+
+    fileManager.createFileAtPath(filePath(key),
+      contents: object.encode(), attributes: nil)
   }
 
   public func object<T: Cachable>(key: String) -> T? {
@@ -27,5 +36,11 @@ public class DiskCache: CacheAware {
   }
 
   public func clear() {
+  }
+
+  // MARK: - Helpers
+
+  private func filePath(key: String) -> String {
+    return "\(path)/key"
   }
 }
