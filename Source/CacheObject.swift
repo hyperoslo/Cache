96ea5221a2e227b39810d3eb1463a8027c8cb82f
Vadym Markov <markov.vadym@gmail.com>
2015-11-12 19:34:34 +0100

    Keys struct

--- Source/CacheObject.swift
+++ Source/CacheObject.swift
@@ -1,9 +1,20 @@
 import Foundation
 
+private struct Keys {
+  static let value = "value"
+  static let expirationDate = "expirationDate"
+}
+
 public class CacheObject<T: AnyObject>: NSObject, NSCoding {
 
   public let value: T
   public let expirationDate: NSDate
+
+  public var expired: Bool {
+    return expirationDate.timeIntervalSinceNow < 0
+  }
+
+  // MARK: - Initialization
 
   public init(value: T, expirationDate: NSDate) {
     self.value = value
@@ -11,20 +22,16 @@ public class CacheObject<T: AnyObject>: NSObject, NSCoding {
   }
 
   public required init?(coder aDecoder: NSCoder) {
-    value = aDecoder.decodeObjectForKey("value") as! T
-    expirationDate = aDecoder.decodeObjectForKey("expirationDate") as! NSDate
+    value = aDecoder.decodeObjectForKey(Keys.value) as! T
+    expirationDate = aDecoder.decodeObjectForKey(Keys.expirationDate) as! NSDate
 
     super.init()
-  }
-
-  public func isExpired() -> Bool {
-    return expirationDate.timeIntervalSinceNow < 0
   }
 
   // MARK: - NSCoding
 
   public func encodeWithCoder(aCoder: NSCoder) {
-    aCoder.encodeObject(value, forKey: "value")
-    aCoder.encodeObject(expirationDate, forKey: "expiryDate")
+    aCoder.encodeObject(value, forKey: Keys.value)
+    aCoder.encodeObject(expirationDate, forKey: Keys.expirationDate)
   }
 }
