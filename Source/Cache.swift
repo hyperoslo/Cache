db5f7bc98301c64e86628422f1c0cd3b7b474636
Vadym Markov <markov.vadym@gmail.com>
2015-11-13 14:54:05 +0100

    Add public methods

--- Source/Cache.swift
+++ Source/Cache.swift
@@ -8,9 +8,26 @@ public class Cache<T: Cachable> {
 
   let name: String
   let kinds: [Kind]
+  let expiry: Expiry = .Never
 
   public init(name: String, kinds: [Kind] = [.Memory, .Disk]) {
     self.name = name
     self.kinds = kinds
+  }
+
+  func add(key: String, object: T, expiry: Expiry = .Never, completion: (() -> Void)?) {
+
+  }
+
+  func object(key: String, completion: (object: T?) -> Void) {
+
+  }
+
+  func remove(key: String, completion: (() -> Void)?) {
+
+  }
+
+  func clear(completion: (() -> Void)?) {
+
   }
 }
