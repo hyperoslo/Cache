2bcc18a5f47940e1ad55bc3e298d2a8b2dce4fe7
Vadym Markov <markov.vadym@gmail.com>
2015-11-13 15:53:01 +0100

    Resole the thingy

--- Source/Cache/CacheFactory.swift
+++ Source/Cache/CacheFactory.swift
@@ -1,9 +1,39 @@
-//
-//  CacheFactory.swift
-//  CacheTests
-//
-//  Created by Vadym Markov on 13/11/15.
-//  Copyright © 2015 Example. All rights reserved.
-//
+public enum CacheKind {
+  case Memory
+  case Disk
+  case Custom(String)
 
-import Foundation
+  public var name: String {
+    let result: String
+
+    switch self {
+    case .Memory:
+      result = "memory"
+    case .Disk:
+      result = "seconds"
+    case .Custom(let name):
+      result = name
+    }
+
+    return result
+  }
+}
+
+public class CacheFactory {
+
+  private static var DefaultCache: CacheAware.Type = MemoryCache.self
+
+  private static var caches: [String: CacheAware.Type] = [
+    CacheKind.Memory.name : MemoryCache.self,
+    CacheKind.Disk.name : DiskCache.self
+  ]
+
+  static func register<T: CacheAware>(kind: String, cache: T.Type) {
+    caches[kind] = cache
+  }
+
+  static func resolve(name: String, kind: CacheKind, maxSize: UInt) -> CacheAware {
+    let Cache: CacheAware.Type = caches[name] ?? DefaultCache
+    return Cache.init(name: name, maxSize: maxSize)
+  }
+}
