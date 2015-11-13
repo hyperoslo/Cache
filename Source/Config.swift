5a7541994c811c6d844a26014bb4c14b30af5a0c
Vadym Markov <markov.vadym@gmail.com>
2015-11-13 16:35:20 +0100

    Improve configuration struct

--- Source/Config.swift
+++ Source/Config.swift
@@ -1,11 +1,15 @@
 public struct Config {
 
+  public let frontKind: CacheKind
+  public let backKind: CacheKind?
   public let expiry: Expiry
   public let maxSize: UInt
 
   // MARK: - Initialization
 
-  public init(expiry: Expiry = .Never, maxSize: UInt = 0) {
+  public init(frontKind: CacheKind, backKind: CacheKind?, expiry: Expiry, maxSize: UInt) {
+    self.frontKind = frontKind
+    self.backKind = backKind
     self.expiry = expiry
     self.maxSize = maxSize
   }
@@ -16,6 +20,10 @@ public struct Config {
 extension Config {
 
   public static var defaultConfig: Config {
-    return Config()
+    return Config(
+      frontKind: .Memory,
+      backKind: .Disk,
+      expiry: .Never,
+      maxSize: 0)
   }
 }
