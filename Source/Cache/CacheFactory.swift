1f7564377dc7710f53b346b92d18cb2ad8a1d6eb
Vadym Markov <markov.vadym@gmail.com>
2015-11-13 20:05:33 +0100

    Move cache kind to a separate file

--- Source/Cache/CacheFactory.swift
+++ Source/Cache/CacheFactory.swift
@@ -1,39 +1,26 @@
-public enum CacheKind {
-  case Memory
-  case Disk
-  case Custom(String)
-
-  public var name: String {
-    let result: String
-
-    switch self {
-    case .Memory:
-      result = "memory"
-    case .Disk:
-      result = "seconds"
-    case .Custom(let name):
-      result = name
-    }
-
-    return result
-  }
-}
-
 public class CacheFactory {
 
   private static var DefaultCache: CacheAware.Type = MemoryCache.self
 
-  private static var caches: [String: CacheAware.Type] = [
+  private static var defaultCaches: [String: CacheAware.Type] = [
     CacheKind.Memory.name : MemoryCache.self,
     CacheKind.Disk.name : DiskCache.self
   ]
 
-  static func register<T: CacheAware>(kind: String, cache: T.Type) {
-    caches[kind] = cache
+  private static var caches = defaultCaches
+
+  // MARK: - Factory
+
+  static func register<T: CacheAware>(kind: CacheKind, cache: T.Type) {
+    caches[kind.name] = cache
   }
 
-  static func resolve(name: String, kind: CacheKind, maxSize: UInt) -> CacheAware {
-    let Cache: CacheAware.Type = caches[name] ?? DefaultCache
+  static func resolve(name: String, kind: CacheKind, maxSize: UInt = 0) -> CacheAware {
+    let Cache: CacheAware.Type = caches[kind.name] ?? DefaultCache
     return Cache.init(name: name, maxSize: maxSize)
+  }
+
+  static func reset() {
+    caches = defaultCaches
   }
 }
