4ed1ea682ab4bfba90af418ac1a667dca68dcffc
Vadym Markov <markov.vadym@gmail.com>
2015-11-13 15:53:14 +0100

    Update config

--- Source/Config.swift
+++ Source/Config.swift
@@ -1,21 +1,13 @@
-public enum CacheKind {
-  case Memory, Disk
-}
-
 public struct Config {
 
-  public let kinds: [CacheKind]
   public let expiry: Expiry
   public let maxSize: UInt
 
   // MARK: - Initialization
 
-  public init(kinds: [CacheKind] = [.Memory, .Disk],
-    expiry: Expiry = .Never,
-    maxSize: UInt = 0) {
-      self.kinds = kinds
-      self.expiry = expiry
-      self.maxSize = maxSize
+  public init(expiry: Expiry = .Never, maxSize: UInt = 0) {
+    self.expiry = expiry
+    self.maxSize = maxSize
   }
 }
 
