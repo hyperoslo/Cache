88772ca6b1ead34e9a9813901de08e78822aac59
Vadym Markov <markov.vadym@gmail.com>
2015-11-13 13:42:44 +0100

    Basic init method with kinds

--- Source/Cache.swift
+++ Source/Cache.swift
@@ -1,10 +1,16 @@
 import Foundation
 
+public enum Kind {
+  case Memory, Disk
+}
+
 public class Cache<T: Cachable> {
 
   let name: String
+  let kinds: [Kind]
 
-  public init(name: String) {
+  public init(name: String, kinds: [Kind] = [.Memory, .Disk]) {
     self.name = name
+    self.kinds = kinds
   }
 }
