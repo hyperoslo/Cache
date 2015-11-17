1208e4faa4274aa89b638a5b870592b717133baf
Vadym Markov <markov.vadym@gmail.com>
2015-11-17 11:07:21 +0100

    Remove unused formats

---
+++ Source/Extensions/JSON+Cache.swift
@@ -0,0 +1,9 @@
+//
+//  JSON+Cache.swift
+//  CacheTests
+//
+//  Created by Vadym Markov on 17/11/15.
+//  Copyright © 2015 Example. All rights reserved.
+//
+
+import Foundation
--- Source/Extensions/NSData+Cache.swift
+++ Source/Extensions/NSData+Cache.swift
@@ -14,29 +14,3 @@ extension NSData: Cachable {
     return self
   }
 }
-
-// MARK: - Helpers
-
-extension NSData {
-
-  enum Format {
-    case PNG, JPEG, Other
-  }
-
-  var format: Format {
-    var result: Format = .Other
-    let pngBytes: [UInt8] = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]
-    let jpgBytes: [UInt8] = [0xFF, 0xD8, 0xFF]
-
-    var buffer = [UInt8](count: 8, repeatedValue: 0)
-    getBytes(&buffer, length: 8)
-
-    if buffer == pngBytes {
-      result = .PNG
-    } else if buffer[0...2] == jpgBytes[0...2] {
-      result = .JPEG
-    }
-
-    return result
-  }
-}
