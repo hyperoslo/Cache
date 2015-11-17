6b78ebc1bca09f8e35bf68bc71ab6170ae2a61c1
Vadym Markov <markov.vadym@gmail.com>
2015-11-17 11:07:36 +0100

    Add JSON extension

--- Source/DataStructures/JSON.swift
+++ Source/DataStructures/JSON.swift
@@ -3,4 +3,17 @@ import Foundation
 public enum JSON {
   case Array([AnyObject])
   case Dictionary([String : AnyObject])
+
+  public var object: AnyObject {
+    var result: AnyObject
+
+    switch self {
+    case .Array(let object):
+      result = object
+    case .Dictionary(let object):
+      result = object
+    }
+
+    return result
+  }
 }
--- Source/Extensions/JSON+Cache.swift
+++ Source/Extensions/JSON+Cache.swift
@@ -1,9 +1,35 @@
-//
-//  JSON+Cache.swift
-//  CacheTests
-//
-//  Created by Vadym Markov on 17/11/15.
-//  Copyright © 2015 Example. All rights reserved.
-//
-
 import Foundation
+
+// MARK: - Cachable
+
+extension JSON: Cachable {
+
+  public typealias CacheType = JSON
+
+  public static func decode(data: NSData) -> CacheType? {
+    var result: CacheType?
+
+    do {
+      let object = try NSJSONSerialization.JSONObjectWithData(data,
+        options: NSJSONReadingOptions())
+
+      switch (object) {
+      case let dictionary as [String : AnyObject]:
+        result = JSON.Dictionary(dictionary)
+      case let array as [AnyObject]:
+        result = JSON.Array(array)
+      default:
+        result = nil
+      }
+    } catch {
+      print("Invalid JSON data")
+    }
+
+    return result
+  }
+
+  public func encode() -> NSData? {
+    return try? NSJSONSerialization.dataWithJSONObject(object,
+      options: NSJSONWritingOptions())
+  }
+}
