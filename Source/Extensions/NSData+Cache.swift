2766bda360d837fca13611e0060117d143689c30
Vadym Markov <markov.vadym@gmail.com>
2015-11-17 10:42:12 +0100

    Cachable data

--- Source/Extensions/NSData+Cache.swift
+++ Source/Extensions/NSData+Cache.swift
@@ -1,5 +1,20 @@
 import Foundation
 
+// MARK: - Cachable
+
+extension NSData: Cachable {
+
+  public typealias CacheType = NSData
+
+  public static func decode(data: NSData) -> CacheType? {
+    return data
+  }
+
+  public func encode() -> NSData? {
+    return self
+  }
+}
+
 // MARK: - Helpers
 
 extension NSData {
