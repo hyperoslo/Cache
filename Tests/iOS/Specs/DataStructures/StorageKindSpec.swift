c9a5f43071f078eacb99e8c82f80d506dafbbcea
Pantelis Zirinis <zirinisp@Panteliss-MacBook-Pro.local>
2017-03-25 17:16:13 -0500

    Add document permanent storage

--- Tests/iOS/Specs/DataStructures/StorageKindSpec.swift
+++ Tests/iOS/Specs/DataStructures/StorageKindSpec.swift
@@ -11,6 +11,7 @@ class StorageKindSpec: QuickSpec {
         it("returns the correct name for default values") {
           expect(StorageKind.memory.name).to(equal("Memory"))
           expect(StorageKind.disk.name).to(equal("Disk"))
+            expect(StorageKind.permanentDisk.name).to(equal("PermanentDisk"))
         }
 
         it("returns the correct name for custom values") {
