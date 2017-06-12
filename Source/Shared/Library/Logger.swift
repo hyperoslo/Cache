81bac21585cedbc1bc9ef45f4b0cbadf4a02a3cc
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-06-12 08:56:31 +0200

    Add public

--- Source/Shared/Library/Logger.swift
+++ Source/Shared/Library/Logger.swift
@@ -4,12 +4,12 @@ import Foundation
 // operation. Usually when ever a method throws. You can eanble `Logger` by setting
 // `Logger.enabled` to `true`. However, if you don't want to debug using print statements
 // you can capture errors by enabling `Swift Error break points`
-class Logger {
+public class Logger {
   // When `Logger` is enabled it will print `Cache` errors to the console.
   // It is disabled by default but you can always catch errors
   // using Swift Error break points even if the feature is
   // disabled.
-  static var isEnabled: Bool = false
+  public static var isEnabled: Bool = false
   static func log(error: Error) {
     guard Logger.isEnabled else {
       return
