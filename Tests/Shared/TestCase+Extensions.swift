1b78539067663dbec4c93e07c6f1358d71995078
Khoa Pham <onmyway133@gmail.com>
2017-09-13 13:05:35 +0200

    Add Given When Then

--- Tests/Shared/TestCase+Extensions.swift
+++ Tests/Shared/TestCase+Extensions.swift
@@ -1,11 +1,15 @@
 import XCTest
 
 extension XCTestCase {
-  func when(_ description: String, closure: () -> Void) {
-    closure()
+  func given(_ description: String, closure: () throws -> Void) throws {
+    try closure()
   }
 
-  func then(_ description: String, closure: () -> Void) {
-    closure()
+  func when(_ description: String, closure: () throws -> Void) throws {
+    try closure()
+  }
+
+  func then(_ description: String, closure: () throws -> Void) throws {
+    try closure()
   }
 }
