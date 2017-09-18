ceb43237d9f4e5d7c4081ca56419ba3b0d990a04
Khoa Pham <onmyway133@gmail.com>
2017-09-18 15:25:13 +0200

    Add ObjectSerializerTests

---
+++ Tests/iOS/Tests/Library/ObjectConverterTests.swift
@@ -0,0 +1,43 @@
+import XCTest
+@testable import Cache
+
+final class ObjectStorageTests: XCTestCase {
+  private var storage: HybridStorage!
+
+  override func setUp() {
+    super.setUp()
+    let memory = MemoryStorage(config: MemoryConfig())
+    let disk = try! DiskStorage(config: DiskConfig(name: "HybridDisk"))
+
+    storage = HybridStorage(memoryStorage: memory, diskStorage: disk)
+  }
+
+  override func tearDown() {
+    try? storage.removeAll()
+    super.tearDown()
+  }
+
+  func testJsonDictionary() throws {
+    let json: [String: Any] = [
+      "first_name": "John",
+      "last_name": "Snow"
+    ]
+
+    let user = try ObjectConverter.convert(json, to: User.self)
+    try storage.setObject(user, forKey: "user")
+
+    let cachedObject = try storage.object(ofType: User.self, forKey: "user")
+    XCTAssertEqual(user, cachedObject)
+  }
+
+  func testJsonString() throws {
+    let string: String = "{\"first_name\": \"John\", \"last_name\": \"Snow\"}"
+
+    let user = try ObjectConverter.convert(string, to: User.self)
+    try storage.setObject(user, forKey: "user")
+
+    let cachedObject = try storage.object(ofType: User.self, forKey: "user")
+    XCTAssertEqual(cachedObject.firstName, "John")
+  }
+}
+
