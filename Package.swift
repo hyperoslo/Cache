d9c3c9150d8c5e9b2ada7c2902de220adfdf7575
Stuart Tett <stuart.tett@gmail.com>
2017-11-16 15:33:28 -0800

    Get it working without images

--- Package.swift
+++ Package.swift
@@ -1,22 +1,30 @@
 // swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
-var package = Package(
+let package = Package(
     name: "Cache",
     products: [
-      .library(
-        name: "Cache",
-        targets: ["Cache"]
-      )
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Cache",
+            targets: ["Cache"]),
     ],
     dependencies: [
-       .package(url: "https://github.com/onmyway133/SwiftHash.git", .upToNextMinor(from: "2.0.0")),
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/onmyway133/SwiftHash.git", .upToNextMinor(from: "2.0.0")),
     ],
     targets: [
-      .target(
-        name: "Cache",
-        dependencies: ["Cache"]
-        )
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Cache",
+            dependencies: ["SwiftHash"],
+            path: "Source/Shared"),
+        .testTarget(
+            name: "CacheTests",
+            dependencies: ["Cache"],
+            path: "Tests"),
     ]
 )
