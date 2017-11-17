a684e639a74d78e563f2cb7471ec82d09589e932
Stuart Tett <stuart.tett@gmail.com>
2017-11-17 11:03:02 -0800

    Exclude ImagWrapper.swift and remove SwiftHash as dependency

--- Package.swift
+++ Package.swift
@@ -13,15 +13,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/onmyway133/SwiftHash.git", .upToNextMinor(from: "2.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Cache",
-            dependencies: ["SwiftHash"],
-            path: "Source/Shared"),
+            path: "Source/Shared",
+            exclude: ["Library/ImageWrapper.swift"]), // relative to the target path
         .testTarget(
             name: "CacheTests",
             dependencies: ["Cache"],
