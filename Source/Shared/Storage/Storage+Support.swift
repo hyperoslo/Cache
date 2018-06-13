74f9794d228d07aa709ccd2481e7978cf98da021
Khoa Pham <onmyway133@gmail.com>
2018-06-13 12:10:55 +0200

    Fix type annotation

--- Source/Shared/Storage/Storage+Support.swift
+++ Source/Shared/Storage/Storage+Support.swift
@@ -1,12 +1,18 @@
 import Foundation
 
 public extension Storage2 {
-  func supportImage() -> Storage2<Image> {
-    let storage = support(transformer: Transformer.imageTransformer())
+  func supportData() -> Storage2<Data> {
+    let storage = support(transformer: Transformer<Data>.forData())
     return storage
   }
 
-  func supportCodable() -> Storage2<Codable> {
-    fatalError()
+  func supportImage() -> Storage2<Image> {
+    let storage = support(transformer: Transformer<Image>.forImage())
+    return storage
+  }
+
+  func supportCodable<U: Codable>(ofType: U.Type) -> Storage2<U> {
+    let storage = support(transformer: Transformer<U>.forCodable(ofType: U.self))
+    return storage
   }
 }
