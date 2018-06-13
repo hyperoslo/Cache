4c48dfb08453f1be109551cc229456148f92669c
Khoa Pham <onmyway133@gmail.com>
2018-06-13 10:36:20 +0200

    Add AsyncStorage2

---
+++ Source/Shared/Storage/AsyncStorage2.swift
@@ -0,0 +1,118 @@
+import Foundation
+import Dispatch
+
+/// Manipulate storage in a "all async" manner.
+/// The completion closure will be called when operation completes.
+public class AsyncStorage2<T> {
+  fileprivate let innerStorage: HybridStorage2<T>
+  public let serialQueue: DispatchQueue
+
+  init(storage: HybridStorage2<T>, serialQueue: DispatchQueue) {
+    self.innerStorage = storage
+    self.serialQueue = serialQueue
+  }
+}
+
+extension AsyncStorage2 {
+  public func entry(forKey key: String, completion: @escaping (Result<Entry2<T>>) -> Void) {
+    serialQueue.async { [weak self] in
+      guard let `self` = self else {
+        completion(Result.error(StorageError.deallocated))
+        return
+      }
+
+      do {
+        let anEntry = try self.innerStorage.entry(forKey: key)
+        completion(Result.value(anEntry))
+      } catch {
+        completion(Result.error(error))
+      }
+    }
+  }
+
+  public func removeObject(forKey key: String, completion: @escaping (Result<()>) -> Void) {
+    serialQueue.async { [weak self] in
+      guard let `self` = self else {
+        completion(Result.error(StorageError.deallocated))
+        return
+      }
+
+      do {
+        try self.innerStorage.removeObject(forKey: key)
+        completion(Result.value(()))
+      } catch {
+        completion(Result.error(error))
+      }
+    }
+  }
+
+  public func setObject(
+    _ object: T,
+    forKey key: String,
+    expiry: Expiry? = nil,
+    completion: @escaping (Result<()>) -> Void) {
+    serialQueue.async { [weak self] in
+      guard let `self` = self else {
+        completion(Result.error(StorageError.deallocated))
+        return
+      }
+
+      do {
+        try self.innerStorage.setObject(object, forKey: key, expiry: expiry)
+        completion(Result.value(()))
+      } catch {
+        completion(Result.error(error))
+      }
+    }
+  }
+
+  public func removeAll(completion: @escaping (Result<()>) -> Void) {
+    serialQueue.async { [weak self] in
+      guard let `self` = self else {
+        completion(Result.error(StorageError.deallocated))
+        return
+      }
+
+      do {
+        try self.innerStorage.removeAll()
+        completion(Result.value(()))
+      } catch {
+        completion(Result.error(error))
+      }
+    }
+  }
+
+  public func removeExpiredObjects(completion: @escaping (Result<()>) -> Void) {
+    serialQueue.async { [weak self] in
+      guard let `self` = self else {
+        completion(Result.error(StorageError.deallocated))
+        return
+      }
+
+      do {
+        try self.innerStorage.removeExpiredObjects()
+        completion(Result.value(()))
+      } catch {
+        completion(Result.error(error))
+      }
+    }
+  }
+
+  func object(forKey key: String, completion: @escaping (Result<T>) -> Void) {
+    entry(forKey: key, completion: { (result: Result<Entry2<T>>) in
+      completion(result.map({ entry in
+        return entry.object
+      }))
+    })
+  }
+
+  func existsObject(
+    forKey key: String,
+    completion: @escaping (Result<Bool>) -> Void) {
+    object(forKey: key, completion: { (result: Result<T>) in
+      completion(result.map({ _ in
+        return true
+      }))
+    })
+  }
+}
