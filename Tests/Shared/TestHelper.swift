e372b096258fef9445df682fea2475692d92d7d8
mohamed <mohamed@keecker.com>
2019-01-31 16:23:34 +0100

    Update to Xcode 10.2 and Swift 5.0

--- Tests/Shared/TestHelper.swift
+++ Tests/Shared/TestHelper.swift
@@ -17,8 +17,8 @@ struct TestHelper {
     NotificationCenter.default.post(name: UIApplication.didEnterBackgroundNotification, object: nil)
     NotificationCenter.default.post(name: UIApplication.willTerminateNotification, object: nil)
     #elseif os(tvOS)
-    NotificationCenter.default.post(name: Notification.Name.UIApplicationDidEnterBackground, object: nil)
-    NotificationCenter.default.post(name: Notification.Name.UIApplicationWillTerminate, object: nil)
+    NotificationCenter.default.post(name: Notification.Name.UIApplication.didEnterBackgroundNotification, object: nil)
+    NotificationCenter.default.post(name: Notification.Name.UIApplication.willTerminateNotification, object: nil)
     #else
       NotificationCenter.default.post(name: NSApplication.willTerminateNotification, object: nil)
       NotificationCenter.default.post(name: NSApplication.didResignActiveNotification, object: nil)
