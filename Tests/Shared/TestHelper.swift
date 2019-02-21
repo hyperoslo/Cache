b38a9669cdd0de5b77ad114c99a488b4b90f6e23
Michael Sena <michsena@amazon.com>
2019-02-21 12:37:32 -0800

    Fix tests
    ** Problem **
    * Some tests were failing to compile due to dead code.
    
    ** Solution **
    * Removed Dead code

--- Tests/Shared/TestHelper.swift
+++ Tests/Shared/TestHelper.swift
@@ -11,17 +11,4 @@ struct TestHelper {
     let buffer = [UInt8](repeating: 0, count: length)
     return Data(bytes: buffer)
   }
-
-  static func triggerApplicationEvents() {
-    #if (iOS)
-    NotificationCenter.default.post(name: UIApplication.didEnterBackgroundNotification, object: nil)
-    NotificationCenter.default.post(name: UIApplication.willTerminateNotification, object: nil)
-    #elseif os(tvOS)
-    NotificationCenter.default.post(name: Notification.Name.UIApplicationDidEnterBackground, object: nil)
-    NotificationCenter.default.post(name: Notification.Name.UIApplicationWillTerminate, object: nil)
-    #else
-      NotificationCenter.default.post(name: NSApplication.willTerminateNotification, object: nil)
-      NotificationCenter.default.post(name: NSApplication.didResignActiveNotification, object: nil)
-    #endif
-  }
 }
